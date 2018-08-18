3277b745806d9f06f087c9b839e82d02f28eb831
Hugo Mota <hugo.txt@gmail.com>
2018-08-18 14:26:20 -0300

    Move server tests to the api package

--- api/server_test.go
+++ api/server_test.go
@@ -1,10 +1,9 @@
-package status
+package api
 
 import (
     "io/ioutil"
     "net/http"
     "net/http/httptest"
-    "nts/api"
     "os"
     "testing"
 )
@@ -12,23 +11,16 @@ import (
 var server *httptest.Server
 
 func TestMain(m *testing.M) {
-    server = httptest.NewServer(http.HandlerFunc(api.Handler))
+    server = httptest.NewServer(http.HandlerFunc(Handler))
     defer server.Close()
     os.Exit(m.Run())
 }
 
 func TestStatus(t *testing.T) {
     response, _ := http.Get(server.URL)
-    if (response.StatusCode != 200) {
-        t.Fail()
-    }
-}
-
-func TestBody(t *testing.T) {
-    response, _ := http.Get(server.URL)
     body, _ := ioutil.ReadAll(response.Body)
     text := string(body)
-    if (text != "OK") {
+    if (response.StatusCode != 200 || text != "OK") {
         t.Fail()
     }
 }
