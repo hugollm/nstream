2c9dea5b82e98a7426570e621726304f2da4d6f6
Hugo Mota <hugo.txt@gmail.com>
2018-08-17 22:58:00 -0300

    Refactor endpoint tests to share a test server object

--- tests/status_test.go
+++ tests/status_test.go
@@ -5,12 +5,19 @@ import (
     "net/http"
     "net/http/httptest"
     "nts/api"
+    "os"
     "testing"
 )
 
-func TestStatus(t *testing.T) {
-    server := httptest.NewServer(http.HandlerFunc(api.Handler))
+var server *httptest.Server
+
+func TestMain(m *testing.M) {
+    server = httptest.NewServer(http.HandlerFunc(api.Handler))
     defer server.Close()
+    os.Exit(m.Run())
+}
+
+func TestStatus(t *testing.T) {
     response, _ := http.Get(server.URL)
     if (response.StatusCode != 200) {
         t.Fail()
@@ -18,8 +25,6 @@ func TestStatus(t *testing.T) {
 }
 
 func TestBody(t *testing.T) {
-    server := httptest.NewServer(http.HandlerFunc(api.Handler))
-    defer server.Close()
     response, _ := http.Get(server.URL)
     body, _ := ioutil.ReadAll(response.Body)
     text := string(body)
