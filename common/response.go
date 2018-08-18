7501441c0453a99a7be16e555d38a9ea7708b9c8
Hugo Mota <hugo.txt@gmail.com>
2018-08-17 22:08:11 -0300

    Update response wrapper to write directly without the fmt package

--- common/response.go
+++ common/response.go
@@ -1,14 +1,11 @@
 package common
 
-import (
-    "fmt"
-    "net/http"
-)
+import "net/http"
 
 type Response struct {
     Response http.ResponseWriter
 }
 
 func (r Response) WriteString(s string) {
-    fmt.Fprint(r.Response, s)
+    r.Response.Write([]byte(s))
 }
