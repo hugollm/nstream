8563d614b3eed3dbaf2bc2d98306a3a31736d4ec
Hugo Mota <hugo.txt@gmail.com>
2018-08-19 18:31:38 -0300

    Refactor signup validation to use simpler funtions for each step

--- signup/signup.go
+++ signup/signup.go
@@ -1,10 +1,11 @@
 package signup
 
 import (
+    "errors"
     "encoding/json"
     "net/http"
     "net/mail"
-    "nts/errors"
+    ntserrors "nts/errors"
     "nts/users"
     "strings"
 )
@@ -29,30 +30,54 @@ func (s Signup) Handle (request *http.Request, response http.ResponseWriter) {
     users.AddUser(input.Email, input.Password)
 }
 
-func validateRequest(request *http.Request) (SignupInput, errors.ApiError) {
+func validateRequest(request *http.Request) (SignupInput, ntserrors.ApiError) {
+    input, jsonErr := validateJson(request)
+    if jsonErr != nil {
+        return input, ntserrors.InvalidJson()
+    }
+    vEmail, emailErr := validateEmail(input.Email)
+    if emailErr != nil {
+        return input, ntserrors.ValidationError(emailErr.Error())
+    }
+    vPassword, passwordErr := validatePassword(input.Password)
+    if passwordErr != nil {
+        return input, ntserrors.ValidationError(passwordErr.Error())
+    }
+    return SignupInput{vEmail, vPassword}, ntserrors.Empty()
+}
+
+func validateJson(request *http.Request) (SignupInput, error) {
     var input SignupInput
     jsonErr := json.NewDecoder(request.Body).Decode(&input)
     if jsonErr != nil {
-        return input, errors.InvalidJson()
+        return input, errors.New("Invalid JSON.")
     }
-    input.Email = strings.TrimSpace(input.Email)
-    if input.Email == "" {
-        return input, errors.ValidationError("Email is required.")
+    return input, nil
+}
+
+func validateEmail(email string) (string, error) {
+    email = strings.TrimSpace(email)
+    if email == "" {
+        return email, errors.New("Email is required.")
     }
-    email, emailErr := mail.ParseAddress(input.Email)
-    if emailErr != nil {
-        return input, errors.ValidationError("Invalid email.")
+    parsed, parseErr := mail.ParseAddress(email)
+    if parseErr != nil {
+        return email, errors.New("Invalid email.")
     }
-    input.Email = email.Address
-    if input.Password == "" {
-        return input, errors.ValidationError("Password is required.")
-    }
-    if len(input.Password) < 8 {
-        return input, errors.ValidationError("Password must be at least 8 characters long.")
-    }
-    _, getErr := users.GetUserByEmail(input.Email)
+    email = parsed.Address
+    _, getErr := users.GetUserByEmail(email)
     if getErr == nil {
-        return input, errors.ValidationError("Email is already taken.")
+        return email, errors.New("Email is already taken.")
     }
-    return input, errors.Empty()
+    return email, nil
+}
+
+func validatePassword(password string) (string, error) {
+    if password == "" {
+        return password, errors.New("Password is required.")
+    }
+    if len(password) < 8 {
+        return password, errors.New("Password must be at least 8 characters long.")
+    }
+    return password, nil
 }
