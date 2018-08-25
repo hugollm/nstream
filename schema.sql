32fef657db64e5ef8e3017c9280784e955a345f2
Hugo Mota <hugo.txt@gmail.com>
2018-08-25 14:30:57 -0300

    Add created_at fields to users and sessions tables on schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,13 @@
 create table if not exists users (
     id bigserial primary key,
     email varchar(254) unique,
-    password varchar(60)
+    password varchar(60),
+    created_at timestamp with time zone default current_timestamp
 );
 
 create table if not exists sessions (
     id bigserial primary key,
     user_id bigint references users (id) on delete cascade,
-    token varchar(64)
+    token varchar(64),
+    created_at timestamp with time zone default current_timestamp
 );
