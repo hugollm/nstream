d6f725be2050ade880d2ff209550f31457ae71cf
Hugo Mota <hugo.txt@gmail.com>
2018-08-25 14:27:09 -0300

    Update database schema to improve user types and support sessions

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 create table if not exists users (
-    id serial,
-    email varchar(255) unique,
+    id bigserial primary key,
+    email varchar(254) unique,
     password varchar(60)
+);
+
+create table if not exists sessions (
+    id bigserial primary key,
+    user_id bigint references users (id) on delete cascade,
+    token varchar(64)
 );
