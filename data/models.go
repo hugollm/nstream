509829c672893bd1c342d8329df5e23bc613113c
Hugo Mota <hugo.txt@gmail.com>
2018-09-01 13:57:51 -0300

    Add json annotations to data models
    With this they can be more easily used as output from the endpoints.

--- data/models.go
+++ data/models.go
@@ -3,22 +3,22 @@ package data
 import "time"
 
 type User struct {
-	Id        int
-	Email     string
-	Password  string
-	CreatedAt time.Time
+	Id        int       `json:"id"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Session struct {
-	Id        int
-	UserId    int
-	Token     string
-	CreatedAt time.Time
+	Id        int       `json:"id"`
+	UserId    int       `json:"user_id"`
+	Token     string    `json:"token"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Note struct {
-	Id        int
-	UserId    int
-	Content   string
-	CreatedAt time.Time
+	Id        int       `json:"id"`
+	UserId    int       `json:"user_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
